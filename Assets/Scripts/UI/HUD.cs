7b2a50e45dbe37c0c1a549676dcb95af36bdd0f7
thief01 <pst12345@wp.pl>
2022-09-19 20:20:43 +0200

    add UI

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game;
+using Platform;
+using TMPro;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI score;
+
+    [SerializeField] private TextMeshProUGUI level;
+
+    [SerializeField] private TextMeshProUGUI lifes;
+
+    [SerializeField] private TextMeshProUGUI bullets;
+
+
+    [SerializeField] private PlatformWeapon platformWeapon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = GameManager.Instance.CurrentPoints.ToString();
+        level.text = GameManager.Instance.CurrentLevel.ToString();
+        lifes.text = GameManager.Instance.CurrentLifes.ToString();
+        bullets.text = platformWeapon.AmmoLeft.ToString();
+    }
+}
