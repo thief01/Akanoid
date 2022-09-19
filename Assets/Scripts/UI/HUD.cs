534057c5ffa1434685cde533e650e978dc13348e
thief01 <pst12345@wp.pl>
2022-09-19 20:24:35 +0200

    add namespaces

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,29 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using Game;
 using Platform;
 using TMPro;
 using UnityEngine;
 
-public class HUD : MonoBehaviour
+namespace UI
 {
-    [SerializeField] private TextMeshProUGUI score;
-
-    [SerializeField] private TextMeshProUGUI level;
-
-    [SerializeField] private TextMeshProUGUI lifes;
-
-    [SerializeField] private TextMeshProUGUI bullets;
-
-
-    [SerializeField] private PlatformWeapon platformWeapon;
-
-    // Update is called once per frame
-    void Update()
+    public class HUD : MonoBehaviour
     {
-        score.text = GameManager.Instance.CurrentPoints.ToString();
-        level.text = GameManager.Instance.CurrentLevel.ToString();
-        lifes.text = GameManager.Instance.CurrentLifes.ToString();
-        bullets.text = platformWeapon.AmmoLeft.ToString();
+        [SerializeField] private TextMeshProUGUI score;
+
+        [SerializeField] private TextMeshProUGUI level;
+
+        [SerializeField] private TextMeshProUGUI lifes;
+
+        [SerializeField] private TextMeshProUGUI bullets;
+
+
+        [SerializeField] private PlatformWeapon platformWeapon;
+
+        // Update is called once per frame
+        void Update()
+        {
+            score.text = GameManager.Instance.CurrentPoints.ToString();
+            level.text = GameManager.Instance.CurrentLevel.ToString();
+            lifes.text = GameManager.Instance.CurrentLifes.ToString();
+            bullets.text = platformWeapon.AmmoLeft.ToString();
+        }
     }
 }
