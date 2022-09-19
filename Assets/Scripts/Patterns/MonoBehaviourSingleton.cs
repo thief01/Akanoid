f86c6f6efe3760bcd44cd0d1dd0c5d630d5b1e8a
thief01 <pst12345@wp.pl>
2022-09-19 18:27:38 +0200

    patterns

---
+++ Assets/Scripts/Patterns/MonoBehaviourSingleton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Patterns
+{
+    public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (instace == null)
+                {
+                    instace = FindObjectOfType<T>();
+                    if (instace == null)
+                    {
+                        GameObject g = new GameObject();
+                        instace=g.AddComponent<T>();
+                    }
+                }
+
+                return instace;
+            }
+        }
+
+        private static T instace;
+    }
+}
---
+++ Assets/Scripts/Patterns/Pool.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Patterns
+{
+    public class Pool<T>  where T : MonoBehaviour
+    {
+        private readonly Vector3 SAFE_POINT = new Vector3(-25, 0, 0);
+        public static Pool<T> Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Pool<T>();
+                }
+
+                return instance;
+            }
+        }
+
+        private static Pool<T> instance;
+
+        public int CurrentPoolObjects => pooledObjects.Count;
+
+        public UnityEvent OnObjectDestroyed = new UnityEvent();
+        public UnityEvent OnObjectSpawned = new UnityEvent();
+
+        private List<T> pooledObjects = new List<T>();
+        private GameObject prefab;
+        private Transform poolParrent;
+
+    
+
+        public void InitPool(int items, GameObject gameObject)
+        {
+            if (poolParrent == null)
+            {
+                poolParrent = new GameObject().transform;
+                poolParrent.name = typeof(Pool<T>).ToString();
+            }
+            prefab = gameObject;
+            for (int i = 0; i < items; i++)
+            {
+                GameObject g = GameObject.Instantiate(gameObject, poolParrent);
+                pooledObjects.Add(g.GetComponent<T>());
+                g.SetActive(false);
+            }
+        }
+
+        public T GetObject()
+        {
+            if (pooledObjects.Count == 0)
+            {
+                Debug.LogWarning("No objects in pool");
+                return default(T);
+            }
+
+            OnObjectSpawned.Invoke();
+            T t = pooledObjects[0];
+            t.transform.position = SAFE_POINT;
+            t.gameObject.SetActive(true);
+            pooledObjects.RemoveAt(0);
+            return t;
+        }
+
+        public void BackToPool(T t)
+        {
+            t.gameObject.SetActive(false);
+            pooledObjects.Add(t);
+            OnObjectDestroyed.Invoke();
+        }
+    }
+}
